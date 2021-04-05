ffe5b9ff577224aa8c39127b75dd0af8b4717807
cristian-avendano <cristian-avendano@upc.edu.co>
2021-04-04 23:29:18 -0500

    IReserve-5: Adicion de rutas login ,registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'registro', component: RegistroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
