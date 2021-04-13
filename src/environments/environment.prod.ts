ba37b65e9c66c638a6ec2ef6711c9af857b6c8d8
Juan Pablo Barrera <juanpablo.is@outlook.com>
2021-04-13 17:58:48 -0500

    IReserve-4: se agrega configuración Firebase al proyecto.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBsyVQntxVDtT8_GN56NHV7Ygy9jFEDGBY',
+    authDomain: 'ireserve-e279b.firebaseapp.com',
+    projectId: 'ireserve-e279b',
+    storageBucket: 'ireserve-e279b.appspot.com',
+    messagingSenderId: '144838815253',
+    appId: '1:144838815253:web:eddaabd3452f8d9348281c',
+    measurementId: 'G-BNRJHGVXZ6'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBsyVQntxVDtT8_GN56NHV7Ygy9jFEDGBY',
+    authDomain: 'ireserve-e279b.firebaseapp.com',
+    projectId: 'ireserve-e279b',
+    storageBucket: 'ireserve-e279b.appspot.com',
+    messagingSenderId: '144838815253',
+    appId: '1:144838815253:web:eddaabd3452f8d9348281c',
+    measurementId: 'G-BNRJHGVXZ6'
+  }
 };
 
 /*
