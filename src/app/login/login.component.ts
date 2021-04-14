0a4f54f2af51d59ca061175dd44c3b0e926682c8
Juan Pablo Barrera <juanpablo.is@outlook.com>
2021-04-13 19:15:42 -0500

    IReserve-8: se ajusta formulario login a formulario reactivo.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.buildForm();
   }
 
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.minLength(6)]]
+    });
+  }
+
+  loginUser(event: Event) {
+    event.preventDefault();
+    console.log(this.form.value);
+  }
 }
