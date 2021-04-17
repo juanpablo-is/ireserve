6abc7704c1964aa00a13f23a62866e9b0d13c525
Juan Barrera <juanpablo.is@outlook.com>
2021-04-17 17:15:49 -0500

    IReserve-9: se agrega FormGroup para formulario reactivo Angular.

--- src/app/register-restaurant/register-restaurant.component.ts
+++ src/app/register-restaurant/register-restaurant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register-restaurant',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterRestaurantComponent implements OnInit {
 
+  form: FormGroup;
+  
   constructor() { }
 
   ngOnInit(): void {
