bb60c40cb969e00a583a4e1df39871c642d690df
Juan Barrera <juanpablo.is@outlook.com>
2021-05-02 17:59:50 -0500

    IReserve-24: se listado de categorias de acuerdo a tipo de restaurante.

--- src/app/set-menu/menu-option/menu-item/menu-item.component.ts
+++ src/app/set-menu/menu-option/menu-item/menu-item.component.ts
@@ -1,38 +1,26 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-item',
   templateUrl: './menu-item.component.html',
   styleUrls: ['./menu-item.component.sass']
 })
-export class MenuItemComponent implements OnInit {
+export class MenuItemComponent {
 
-  @ViewChild('mItem') mItem: ElementRef;
-  @ViewChild('show') show: ElementRef;
   @Input() items: any[];
   @Input() userId: string;
-  @Input() name: string;
-  @Input() price: number;
-  @Input() description: string;
-  @Input() urlPhoto: string;
-  isShowed: boolean = true;
+  @Input() data: any;
+  @Input() index: any;
+
+  isShowed = true;
+
   constructor() { }
 
-  ngOnInit(): void {
-    console.log("entra");
-    console.log(this.items)
-  }
-
-  showItem():void{
+  showItem(): void {
     this.isShowed = !this.isShowed;
   }
 
   delete(): void {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name === this.name) {
-        this.items.splice(i, 1);
-        break;
-      }
-    }
+    this.items.splice(this.index, 1);
   }
 }
--- src/app/set-menu/menu-option/menu-option.component.ts
+++ src/app/set-menu/menu-option/menu-option.component.ts
@@ -7,23 +7,20 @@ import { FirebaseStorageService } from 'src/app/services/firebase-storage/fireba
   templateUrl: './menu-option.component.html',
   styleUrls: ['./menu-option.component.sass']
 })
-export class MenuOptionComponent implements OnInit {
-  @Input() items: any;
-  @Input() categories: any;
+export class MenuOptionComponent {
+
+  @Input() items: any = [];
+  @Input() optionName: string;
   @ViewChild('show') eShow: ElementRef;
   @ViewChild('addName') addName: ElementRef;
   @ViewChild('addPrice') addPrice: ElementRef;
   @ViewChild('addDescription') addDescription: ElementRef;
-  optionName: string;
-  isShowed = false;
-  file:any;
-  urlPhoto: string;
-  constructor(
-    private serviceStorage: FirebaseStorageService) {}
+  @ViewChild('addImage') addImage: ElementRef;
 
-  ngOnInit(): void {
-    this.optionName = this.items.categoryName;
-  }
+  isShowed = false;
+  file: any;
+
+  constructor(private serviceStorage: FirebaseStorageService) { }
 
   showListOfItems(): void {
     if (this.isShowed === true) {
@@ -38,29 +35,27 @@ export class MenuOptionComponent implements OnInit {
     const newName = this.addName.nativeElement.value;
     const newPrice = this.addPrice.nativeElement.value;
     const newDescription = this.addDescription.nativeElement.value;
+    const newImage = this.addImage.nativeElement;
+
     if (newName && newPrice) {
-      this.items.categoryValues.push({ name: newName, price: newPrice, description: newDescription, urlPhoto: this.urlPhoto});
-      this.addName.nativeElement.value = '';
-      this.addPrice.nativeElement.value = '';
-      this.addDescription.nativeElement.value = '';
+      if (newImage.files.length > 0) {
+        this.file = newImage.files[0];
+
+        const filePath = `${this.file.name}-${Date.now()}`;
+        const referenceFile = this.serviceStorage.referenceStorage(filePath);
+        const taskUpload = this.serviceStorage.uploadStorage(filePath, this.file);
+        taskUpload.snapshotChanges().pipe(
+          finalize(() => {
+            referenceFile.getDownloadURL().subscribe((url: string) => {
+              this.items.push({ name: newName, price: newPrice, description: newDescription, urlPhoto: url });
+              this.addName.nativeElement.value = '';
+              this.addPrice.nativeElement.value = '';
+              this.addDescription.nativeElement.value = '';
+              this.addImage.nativeElement.value = '';
+            });
+          })
+        ).subscribe();
+      }
     }
-    console.log(this.items);
-  }
-  changeFile(event: any): void {
-    if (event.target.files.length > 0) {
-      this.file = event.target.files[0];
-      console.log(this.file);
-    }
-    const filePath = `${this.file.name}-${Date.now()}`;
-    const referenceFile = this.serviceStorage.referenceStorage(filePath);
-    const taskUpload = this.serviceStorage.uploadStorage(filePath, this.file);
-    taskUpload.snapshotChanges().pipe(
-      finalize(() => {
-        referenceFile.getDownloadURL().subscribe((url: string) => {   
-          this.urlPhoto = url;         
-        });
-      })
-    ).subscribe();
-    alert("imagen subida");
   }
 }
