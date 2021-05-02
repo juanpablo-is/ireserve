c367c2cbd869431ad10b242ccd5ef006c0e35706
Juan Barrera <juanpablo.is@outlook.com>
2021-05-01 22:53:31 -0500

    IReserve-24: se adapta opción de menú de acuerdo a tipo de restaurante.

--- src/app/interfaces/restaurant.ts
+++ src/app/interfaces/restaurant.ts
@@ -1,5 +1,6 @@
 export interface Restaurant {
     idUser: string;
+    type: string;
     name: string;
     address: string;
     dateStart: Date;
--- src/app/set-menu/set-menu.component.ts
+++ src/app/set-menu/set-menu.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { MenuService } from '../services/backend/menu/menu.service';
 
 @Component({
@@ -9,70 +9,52 @@ import { MenuService } from '../services/backend/menu/menu.service';
   styleUrls: ['./set-menu.component.sass']
 })
 
-export class SetMenuComponent implements OnInit {
+export class SetMenuComponent {
 
-  platoFuerte = 'Plato fuerte';
-  platoCorriente = 'Plato Corriente';
-  bebidas = 'Bebidas';
-  entradas = 'Entradas';
-  adicionales = 'Adicionales';
-
+  btnSaveMenu = 'INGRESAR MENU';
   idRestaurant: string;
   name: string;
   form: FormGroup;
+  items: any;
   alertError: string;
-  btnSaveMenu = 'INGRESAR MENU';
-
-  platosFuertes: any[] = [];
-  platosCorrientes: any[] = [];
-  drinks: any[] = [];
-  entrances: any[] = [];
-  additionals: any[] = [];
 
   constructor(
     private menuService: MenuService,
-    private router: Router,
-    private activateRoute: ActivatedRoute
+    private router: Router
   ) {
     const user = JSON.parse(sessionStorage.getItem('user'));
+    if (!user || !user.idRestaurant) { this.router.navigate(['/register']); return; }
 
-    this.activateRoute.queryParams.subscribe(params => {
-      if (!user || !params.idRestaurant) { this.router.navigate(['/register']); return; }
-      this.idRestaurant = params.idRestaurant;
-      this.name = user.firstname;
-    });
+    this.name = user.firstname;
+    this.idRestaurant = user.idRestaurant;
+    this.items = this.getItemsMenu(user.typeRestaurant);
 
-
-    this.menuService.getMenu(this.idRestaurant)
-      .subscribe((menu: any) => {
-        const count = Object.keys(menu).length;
-
-        if (count > 1) {
-          this.platosFuertes = menu.dishes.platosFuertes;
-          this.platosCorrientes = menu.dishes.platosCorrientes;
-          this.drinks = menu.dishes.drinks;
-          this.entrances = menu.dishes.entrances;
-          this.additionals = menu.dishes.additionals;
-        }
-      });
+    // Consultar menu de acuerdo a restaurante para los items.
+    // this.menuService.getMenu(this.idRestaurant)
+    //   .subscribe((menu: any) => {
+    //     const count = Object.keys(menu).length;
+    //     if (count > 1) { }
+    //   });
   }
 
   save(): void {
+    this.alertError = '';
     this.btnSaveMenu = 'CARGANDO...';
+
+    const dishes = this.getItemsMap();
+    if (!dishes) {
+      this.alertError = 'Debe ingresar al menos un producto.';
+      this.btnSaveMenu = 'INGRESAR MENU';
+      return;
+    }
+
     const body = {
-      dishes: {
-        platosFuertes: this.platosFuertes,
-        platosCorrientes: this.platosCorrientes,
-        drinks: this.drinks,
-        entrances: this.entrances,
-        additionals: this.additionals
-      },
+      dishes,
       idRestaurant: this.idRestaurant
     };
 
     this.menuService.addMenuItem(body)
       .then(() => {
-        this.btnSaveMenu = 'INGRESAR MENU';
         this.router.navigate(['/']);
       })
       .catch((e: any) => {
@@ -81,6 +63,49 @@ export class SetMenuComponent implements OnInit {
       });
   }
 
-  ngOnInit(): void {
+  // Convierte la variable 'items' en object para Firebase.
+  getItemsMap(): {} {
+    const map = {};
+    let count = 0;
+    for (const item in this.items) {
+      if (Object.prototype.hasOwnProperty.call(this.items, item)) {
+        if (this.items[item].items.length > 0) {
+          count++;
+        }
+        map[item] = this.items[item].items;
+      }
+    }
+    return count > 0 ? map : false;
+  }
+
+  // Retorna items de acuerdo a tipo de restaurante.
+  getItemsMenu(typeRestaurant: string): any {
+    const items: any = {};
+
+    if (typeRestaurant === 'restaurante') {
+      items.breakfast = { text: 'Desayunos', items: [] };
+      items.platosFuertes = { text: 'Platos fuertes', items: [] };
+      items.platosCorrientes = { text: 'Platos corrientes', items: [] };
+      items.drinks = { text: 'Bebidas', items: [] };
+      items.entraces = { text: 'Entradas', items: [] };
+      items.additionals = { text: 'Adicionales', items: [] };
+      items.desserts = { text: 'Postres', items: [] };
+    } else if (typeRestaurant === 'cafeteria') {
+      items.breakfast = { text: 'Desayunos', items: [] };
+      items.drinks = { text: 'Bebidas', items: [] };
+    } else if (typeRestaurant === 'heladeria') {
+      items.iceCream = { text: 'Helados', items: [] };
+      items.drinks = { text: 'Bebidas', items: [] };
+      items.desserts = { text: 'Postres', items: [] };
+      items.shakes = { text: 'Batidos', items: [] };
+      items.waffles = { text: 'Waffles', items: [] };
+    } else if (typeRestaurant === 'bar') {
+      items.beers = { text: 'Cervezas', items: [] };
+      items.cocktails = { text: 'Cocteles', items: [] };
+      items.wines = { text: 'Vinos', items: [] };
+      items.coffee = { text: 'Café', items: [] };
+    }
+
+    return items;
   }
 }
