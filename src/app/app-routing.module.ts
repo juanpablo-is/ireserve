4e52719345a3b3e3af95f1ccb29b991877c09af1
Juan Barrera <juanpablo.is@outlook.com>
2021-05-02 19:53:07 -0500

    IReserve-19: se crea componente para dashboard Home.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterRestaurantComponent } from './register-restaurant/register-restaurant.component';
 import { RegistroComponent } from './registro/registro.component';
 import { SetMenuComponent } from './set-menu/set-menu.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistroComponent },
   { path: 'register-restaurant', component: RegisterRestaurantComponent },
