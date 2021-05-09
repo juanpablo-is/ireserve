d242b0addf0cc2da623cc818a8b6d26ec0b7ab2e
Juan Barrera <juanpablo.is@outlook.com>
2021-05-08 22:03:46 -0500

    IReserve-34: se ajusta componente bottom navigation para clase activeRouting

--- src/app/home/bottom-navigation/bottom-navigation.component.ts
+++ src/app/home/bottom-navigation/bottom-navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bottom-navigation',
@@ -7,11 +8,22 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BottomNavigationComponent implements OnInit {
 
-  @Input() buttons: any[];
+  @Input() user: any;
+  buttons: any[] = [];
 
-  constructor() { }
+  constructor(public route: Router) { }
 
   ngOnInit(): void {
+    if (this.user.role === 'Cliente') {
+      this.buttons.push({ name: 'Inicio', iconClass: 'fas fa-home', routing: '/' });
+      this.buttons.push({ name: 'Reservación', iconClass: 'far fa-calendar-alt', routing: '/reservations' });
+      this.buttons.push({ name: 'Menú', iconClass: 'fas fa-book-open', routing: '/menu' });
+      this.buttons.push({ name: 'Profile', iconClass: 'fas fa-user-circle', routing: '/profile' });
+    } else {
+      this.buttons.push({ name: 'Inicio', iconClass: 'fas fa-home', routing: '/' });
+      this.buttons.push({ name: 'Reservación', iconClass: 'far fa-calendar-alt', routing: '/reservations' });
+      this.buttons.push({ name: 'Profile', iconClass: 'fas fa-user-circle', routing: '/profile' });
+    }
   }
 
 }
