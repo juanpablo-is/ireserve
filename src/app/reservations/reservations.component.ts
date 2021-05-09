b0257050cba440e41b25a87cf1439884d1876a5a
Juan Barrera <juanpablo.is@outlook.com>
2021-05-09 18:04:49 -0500

    IReserve-34: se crean tarjetas para el historial de reservaciones.

--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,18 +6,81 @@ import { Router } from '@angular/router';
   templateUrl: './reservations.component.html',
   styleUrls: ['./reservations.component.sass']
 })
-export class ReservationsComponent implements OnInit {
+export class ReservationsComponent {
 
   user: any;
+  pending: any[] = [];
+
+  @ViewChildren('itemsPending') itemsPendingElement: QueryList<any>;
+  @ViewChildren('itemsActive') itemsActiveElement: QueryList<any>;
+  @ViewChildren('itemsComplete') itemsCompleteElement: QueryList<any>;
 
   constructor(
     private router: Router
   ) {
     this.user = JSON.parse(sessionStorage.getItem('user'));
     if (!this.user) { this.router.navigate(['/login']); return; }
+    this.pending.push({ restaurant: 'McDo', date: '12/05/2021 12:20pm', dateStart: '10/05/2021', chairs: 5, address: 'Calle 49 #29 L 04 - 3', name: 'Pablo' });
+    this.pending.push({ restaurant: 'McDo', date: '12/05/2021 12:20pm', dateStart: '10/05/2021', chairs: 5, name: 'Juan' });
+    this.pending.push({ restaurant: 'McDo', date: '12/05/2021 12:20pm', dateStart: '10/05/2021', chairs: 5, address: '', name: '' });
+    this.pending.push({ restaurant: 'McDo', date: '12/05/2021 12:20pm', dateStart: '10/05/2021', chairs: 5, address: '', name: '' });
   }
 
-  ngOnInit(): void {
+  // Colapsa acordión de tarjetas.
+  collapsingCard(section: number, index: number): void {
+    switch (section) {
+      case 0: {
+        this.itemsPendingElement.toArray().forEach((element, i) => {
+          const div = element.nativeElement.children[0];
+          const divHidden = element.nativeElement.children[1];
+
+          if (i === index && divHidden.classList.contains('hidden')) {
+            divHidden.classList.remove('hidden');
+            div.querySelector('svg').classList.remove('fa-chevron-down');
+            div.querySelector('svg').classList.add('fa-chevron-up');
+          } else {
+            divHidden.classList.add('hidden');
+            div.querySelector('svg').classList.remove('fa-chevron-up');
+            div.querySelector('svg').classList.add('fa-chevron-down');
+          }
+        });
+        break;
+      }
+      case 1: {
+        this.itemsActiveElement.toArray().forEach((element, i) => {
+          const div = element.nativeElement.children[0];
+          const divHidden = element.nativeElement.children[1];
+
+          if (i === index && divHidden.classList.contains('hidden')) {
+            divHidden.classList.remove('hidden');
+            div.querySelector('svg').classList.remove('fa-chevron-down');
+            div.querySelector('svg').classList.add('fa-chevron-up');
+          } else {
+            divHidden.classList.add('hidden');
+            div.querySelector('svg').classList.remove('fa-chevron-up');
+            div.querySelector('svg').classList.add('fa-chevron-down');
+          }
+        });
+        break;
+      }
+      case 2: {
+        this.itemsCompleteElement.toArray().forEach((element, i) => {
+          const div = element.nativeElement.children[0];
+          const divHidden = element.nativeElement.children[1];
+
+          if (i === index && divHidden.classList.contains('hidden')) {
+            divHidden.classList.remove('hidden');
+            div.querySelector('svg').classList.remove('fa-chevron-down');
+            div.querySelector('svg').classList.add('fa-chevron-up');
+          } else {
+            divHidden.classList.add('hidden');
+            div.querySelector('svg').classList.remove('fa-chevron-up');
+            div.querySelector('svg').classList.add('fa-chevron-down');
+          }
+        });
+        break;
+      }
+    }
   }
 
 }
