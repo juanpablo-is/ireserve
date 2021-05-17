a10d10b4854bea861d5ea6b404a0cbb772ebd6de
Juan Barrera <juanpablo.is@outlook.com>
2021-05-17 12:02:55 -0500

    IReserve-42: se incorporate mapa dentro de modal para seleccionar ubicación del restaurante.

--- src/app/services/frontend/map-custom.service.ts
+++ src/app/services/frontend/map-custom.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapCustomService {
 
-  constructor() { }
+  mapbox = (mapboxgl as typeof mapboxgl);
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 4.632235671307743;
+  long = -74.06666639070954;
+  zoom = 13;
+  coords: any = {};
+
+  constructor() {
+    this.mapbox.accessToken = environment.mapboxKey;
+  }
+
+  /**
+   * Returna promesa para crear mapa con Mapbox.
+   * @returns Promise
+   */
+  buildMap(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try {
+        this.map = new mapboxgl.Map({
+          container: 'map',
+          style: this.style,
+          zoom: this.zoom,
+          center: [this.long, this.lat]
+        });
+
+        const geocoder = new MapboxGeocoder({
+          accessToken: mapboxgl.accessToken,
+          mapboxgl,
+          marker: false,
+          placeholder: 'Buscar ubicación de establecimiento'
+        });
+
+        geocoder.on('result', ({ result }) => {
+          const marker = new mapboxgl.Marker({ draggable: true, color: 'red' });
+          marker.on('dragend', () => {
+            this.coords = { geoPoint: marker.getLngLat(), place: '' };
+          });
+          marker.setLngLat(result.center).addTo(this.map);
+          this.coords = { geoPoint: { lng: result.center[0], lat: result.center[1] }, place: result['place_name_es-ES'] };
+        });
+
+        return resolve({
+          map: this.map,
+          geocoder
+        });
+      } catch (error) {
+        return reject(error);
+      }
+    });
+  }
+
+  /**
+   * Retorna objeto de las coordenas seleccionadas.
+   */
+  points(): void {
+    return this.coords;
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,6 @@ export const environment = {
     appId: '1:144838815253:web:eddaabd3452f8d9348281c',
     measurementId: 'G-BNRJHGVXZ6'
   },
-  urlBackend: 'https://ireserve-backend.herokuapp.com'
+  urlBackend: 'https://ireserve-backend.herokuapp.com',
+  mapboxKey: 'pk.eyJ1IjoianVhbnBhYmxvLWlzIiwiYSI6ImNrYXI3eWVvMTBkdHUyc250eGQ2bDE5Y2YifQ.m4rExvFzYcQHEjCfc_gcCg'
 };
