712f911be7454baae31cb83229fb79c218c20b8e
Juan Pablo Barrera <juanpablo.is@outlook.com>
2021-05-25 11:36:20 -0500

    IReserve-54: se ajusta componente set-menu de acuerdo al componente padre.

--- src/app/set-menu/set-menu.component.ts
+++ src/app/set-menu/set-menu.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { RestService } from '../services/backend/rest.service';
 import { LocalStorageService } from '../services/frontend/local-storage.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-set-menu',
@@ -12,13 +12,14 @@ import { LocalStorageService } from '../services/frontend/local-storage.service'
 
 export class SetMenuComponent {
 
-  btnSaveMenu = 'INGRESAR MENU';
+  @Input() embed: any = false;
+  btnSaveMenu = '';
   idRestaurant: string;
   name: string;
-  form: FormGroup;
   items: any;
   alertError: string;
   type = 'establecimiento';
+  objectKeys = Object.keys;
 
   constructor(
     private restService: RestService,
@@ -30,17 +31,16 @@ export class SetMenuComponent {
 
     this.name = user.firstname;
     this.idRestaurant = user.idRestaurant;
-    this.type = user.typeRestaurant;
-    this.items = this.getItemsMenu(this.type);
+    this.type = user.type;
 
     // Consultar menu de acuerdo a restaurante para los items.
     this.restService.get(`/api/menu?idRestaurant=${this.idRestaurant}`)
       .then((result: { ok: any; status: number; body: any }) => {
         if (result.ok && result.status === 200) {
           const count = Object.keys(result.body).length;
-
+          this.btnSaveMenu = this.embed ? 'EDITAR MENÚ' : 'INGRESAR MENU';
           if (count > 1) {
-            this.items = this.parseItems(result.body.dishes);
+            this.items = result.body.dishes;
           }
         }
       })
@@ -49,92 +49,56 @@ export class SetMenuComponent {
       });
   }
 
+  /**
+   * Guarda o edita el menú del establecimiento.
+   */
   save(): void {
     this.alertError = '';
     this.btnSaveMenu = 'CARGANDO...';
 
-    const dishes = this.getItemsMap();
-    if (!dishes) {
-      this.alertError = 'Debe ingresar al menos un producto.';
-      this.btnSaveMenu = 'INGRESAR MENU';
-      return;
-    }
-
     const body = {
-      dishes,
+      dishes: this.items,
       idRestaurant: this.idRestaurant
     };
 
     this.restService.post('/api/menu', body)
       .then((result: { ok: any; status: number; body: any }) => {
         if (result.ok && result.status === 201) {
-          this.btnSaveMenu = 'INGRESAR MENU';
+          Swal.fire({
+            icon: 'success',
+            title: 'Menú editado',
+            text: this.embed ? `El menu de su ${this.type} se ha editado.` : `El menu de su ${this.type} se ha guardado.`,
+            confirmButtonText: 'Cerrar'
+          });
+          this.btnSaveMenu = this.embed ? 'EDITAR MENÚ' : 'INGRESAR MENU';
           this.router.navigate(['/']);
         }
       })
       .catch((e: any) => {
         this.alertError = e.error.response || 'Se ha presentado un error, intente nuevamente.';
-        this.btnSaveMenu = 'INGRESAR MENU';
+        this.btnSaveMenu = this.embed ? 'EDITAR MENÚ' : 'INGRESAR MENU';
       });
   }
 
-  // Convierte la variable 'items' en object para Firebase.
-  getItemsMap(): {} {
-    const map = {};
-    let count = 0;
-    for (const item in this.items) {
-      if (Object.prototype.hasOwnProperty.call(this.items, item)) {
-        if (this.items[item].items.length > 0) {
-          count++;
-        }
-
-        map[this.items[item].key] = this.items[item].items;
-      }
-    }
-    return count > 0 ? map : false;
-  }
-
-  // Retorna items de acuerdo a tipo de restaurante.
-  getItemsMenu(typeRestaurant: string): any {
+  // Retorna traducción de la categoria del menú.
+  getTranslate(key: string): any {
     const items: any = [];
 
-    if (typeRestaurant === 'restaurante') {
-      items.push({ key: 'breakfast', text: 'Desayunos', items: [] });
-      items.push({ key: 'platosFuertes', text: 'Platos fuertes', items: [] });
-      items.push({ key: 'platosCorrientes', text: 'Platos corrientes', items: [] });
-      items.push({ key: 'drinks', text: 'Bebidas', items: [] });
-      items.push({ key: 'entraces', text: 'Entradas', items: [] });
-      items.push({ key: 'additionals', text: 'Adicionales', items: [] });
-      items.push({ key: 'desserts', text: 'Postres', items: [] });
-    } else if (typeRestaurant === 'cafeteria') {
-      items.push({ key: 'breakfast', text: 'Desayunos', items: [] });
-      items.push({ key: 'drinks', text: 'Bebidas', items: [] });
-    } else if (typeRestaurant === 'heladeria') {
-      items.push({ key: 'iceCream', text: 'Helados', items: [] });
-      items.push({ key: 'drinks', text: 'Bebidas', items: [] });
-      items.push({ key: 'desserts', text: 'Postres', items: [] });
-      items.push({ key: 'shakes', text: 'Batidos', items: [] });
-      items.push({ key: 'waffles', text: 'Waffles', items: [] });
-    } else if (typeRestaurant === 'bar') {
-      items.push({ key: 'beers', text: 'Cervezas', items: [] });
-      items.push({ key: 'cocktails', text: 'Cocteles', items: [] });
-      items.push({ key: 'wines', text: 'Vinos', items: [] });
-      items.push({ key: 'coffee', text: 'Café', items: [] });
-    }
+    items.breakfast = 'Desayunos';
+    items.platosFuertes = 'Platos fuertes';
+    items.platosCorrientes = 'Platos corrientes';
+    items.drinks = 'Bebidas';
+    items.entraces = 'Entradas';
+    items.additionals = 'Adicionales';
+    items.desserts = 'Postres';
+    items.iceCream = 'Helados';
+    items.shakes = 'Batidos';
+    items.waffles = 'Waffles';
+    items.beers = 'Cervezas';
+    items.cocktails = 'Cocteles';
+    items.wines = 'Vinos';
+    items.coffee = 'Café';
 
-    return items;
-  }
-
-  // Parsea los items de DB de acuerdo a la estructura de categorias.
-  parseItems(menu): any[] {
-    const newItems = [];
-
-    for (const item in menu) {
-      if (Object.prototype.hasOwnProperty.call(menu, item)) {
-        const da = this.items.filter(it => it.key === item)[0];
-        newItems.push({ text: da.text, items: menu[item] });
-      }
-    }
-    return newItems;
+    return items[key] ?? '';
   }
 }
